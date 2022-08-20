33198f782c415bf7fd46eb0588406b70b58fe8a3
marinellirubens <marinelli.rubens@gmail.com>
2022-08-20 15:47:36 -0300

    Included logs module

---
+++ config/logs.py
@@ -0,0 +1,29 @@
+"""Method to handle log creation.
+
+DEPRECATED
+    Use of logs is now on gunicorn configuration file gunicorn.conf.py
+
+"""
+# import logging
+# import logging.handlers
+
+
+def set_logger(app, log_dir):
+
+    # log_file_name = os.path.join(log_dir, 'api.log')
+    # formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    # handler = logging.handlers.RotatingFileHandler(log_file_name, maxBytes=1024 * 1000,
+    #                                                backupCount=10)
+
+    # handler.setFormatter(formatter)
+
+    # app.logger.name = 'api'
+
+    # terminal_handler = logging.StreamHandler()
+    # terminal_handler.setFormatter(formatter)
+
+    # app.logger.setLevel(logging.DEBUG)
+    # app.logger.setLevel(eval('logging.{}'.format("DEBUG")))
+    # app.logger.addHandler(handler)
+    # app.logger.addHandler(terminal_handler)
+    app.logger.info('Logger set up' + app.logger.name)
