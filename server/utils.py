b49da91ea7b3ebd348c99fe5a644bb1816874839
marinellirubens <marinelli.rubens@gmail.com>
2022-08-20 20:53:17 -0300

    Included utils module on server

---
+++ server/utils.py
@@ -0,0 +1,40 @@
+
+from config.utils import create_directories
+from database.utils import initiate_db
+from flask import Flask
+from flask_restful import Api
+
+from server import resources
+from server.app import App
+
+
+def create_app(app_name: str, database_directory: str = 'sqlite',
+               log_dir: str = './logs', directory_files: str = './files') -> Flask:
+    """Method to handle requests to the server."""
+    create_directories([database_directory, directory_files, log_dir])
+
+    app = App(app_name)
+    api = Api(app)
+
+    engine, session = initiate_db(database_directory)
+    app.engine = engine
+    app.session = session
+
+    api.add_resource(resources.DatabaseType,
+                     '/database_type/<param>',
+                     methods=['GET', 'POST', 'DELETE'])
+    api.add_resource(resources.DatabaseTypes, '/database_type/', '/', methods=['GET'])
+
+    api.add_resource(resources.ConnectionTypes, '/connection_type/', methods=['GET'])
+    api.add_resource(resources.ConnectionType, '/connection_type/<param>', methods=['GET', 'POST'])
+
+    api.add_resource(resources.ServerTypes, '/server_type/', methods=['GET'])
+    api.add_resource(resources.ServerType, '/server_type/<param>', methods=['GET', 'POST'])
+
+    api.add_resource(resources.FunctionTypes, '/function_type/', methods=['GET'])
+    api.add_resource(resources.FunctionType, '/function_type/<param>', methods=['GET', 'POST'])
+
+    api.add_resource(resources.Groups, '/group/', methods=['GET'])
+    api.add_resource(resources.Group, '/group/<param>', methods=['GET', 'POST'])
+
+    return app
