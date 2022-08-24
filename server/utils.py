dde4035d76f4a8338342271f3a8af524405fd2ba
marinellirubens <marinelli.rubens@gmail.com>
2022-08-23 22:06:27 -0300

    Changed routes for specific id

--- server/utils.py
+++ server/utils.py
@@ -6,7 +6,7 @@ from flask_restful import Api
 
 from server import resources
 from server.app import App
-
+    
 
 def create_app(app_name: str, database_directory: str = 'sqlite',
                log_dir: str = './logs', directory_files: str = './files') -> Flask:
@@ -23,21 +23,22 @@ def create_app(app_name: str, database_directory: str = 'sqlite',
     api.add_resource(resources.DatabaseType,
                      '/database_type/<param>',
                      methods=['GET', 'POST', 'DELETE'])
-    api.add_resource(resources.DatabaseTypes, '/database_type/', methods=['GET'])
+    api.add_resource(resources.DatabaseTypes, '/database_types/', methods=['GET'])
 
-    api.add_resource(resources.ConnectionTypes, '/connection_type/', methods=['GET'])
+    api.add_resource(resources.ConnectionTypes, '/connection_types/', methods=['GET'])
     api.add_resource(resources.ConnectionType, '/connection_type/<param>', methods=['GET', 'POST'])
 
-    api.add_resource(resources.ServerTypes, '/server_type/', methods=['GET'])
+    api.add_resource(resources.ServerTypes, '/server_types/', methods=['GET'])
     api.add_resource(resources.ServerType, '/server_type/<param>', methods=['GET', 'POST'])
 
-    api.add_resource(resources.FunctionTypes, '/function_type/', methods=['GET'])
+    api.add_resource(resources.FunctionTypes, '/function_types/', methods=['GET'])
     api.add_resource(resources.FunctionType, '/function_type/<param>', methods=['GET', 'POST'])
 
-    api.add_resource(resources.Groups, '/group/', methods=['GET'])
+    api.add_resource(resources.Groups, '/groups/', methods=['GET'])
     api.add_resource(resources.Group, '/group/<param>', methods=['GET', 'POST'])
 
     api.add_resource(resources.Users, '/users/', methods=['GET'])
+    api.add_resource(resources.User, '/user/<param>', methods=['GET', 'OPTIONS'])
     
     api.add_resource(resources.UserGroups, '/user_groups/', methods=['GET'])
 
