bda60d03b4ca7358944768c047cfcd3595667ce6
marinellirubens <marinelli.rubens@gmail.com>
2022-08-25 05:55:58 -0300

    Implemented methods for REST basic classes

--- server/resources.py
+++ server/resources.py
@@ -1,18 +1,14 @@
 """Method to handle the resource for the API"""
-# import os
-# import uuid
-
 import re
-from abc import abstractmethod
+from abc import ABC
 from typing import Tuple
+import json
+
 
 from database import models
 from flask import Flask
-from flask import jsonify
 from flask import request
-# from flask import Response, request, send_file
 from flask_restful import Resource
-from sqlalchemy import func
 
 from server.app import App
 from server.authentication import auth
@@ -23,7 +19,7 @@ app: Flask = App('main')
 
 # TODO: Implement other verbs on the resources
 
-class BasicTypes(Resource):
+class BasicTypes(Resource, ABC):
     model_class = None
 
     @auth.login_required
@@ -36,53 +32,107 @@ class BasicTypes(Resource):
         resp = {self.model_class.__tablename__: [row.to_json() for row in rows]}
         return resp
 
+    @auth.login_required
+    def post(self):
+        """Method to handle post requests for type tables."""
+        data = request.get_data()
 
-class BasicTypeSingle(Resource):
+        if 'description' not in data:
+            return {'error': 'Invalid description provided'}, 401
+
+        row = app.session.query(self.model_class).\
+            where(self.model_class.description == data['description']).first()
+
+        if row:
+            return dict(
+               error=f'{self.model_class.__class__.__name__}: {data["description"]} already exists'
+            ), 401
+
+        app.session.bulk_save_objects([self.model_class(data["description"])])
+        app.session.commit()
+
+        row = app.session.query(self.model_class).\
+            where(self.model_class.description == data['description']).first()
+
+        return {'success': 'Registered successfully', 'id': row.id}
+
+    @auth.login_required
+    def options(self):
+        """Method to handle options requests for type tables."""
+        return dict(Allow=['POST', 'GET'])
+
+
+class BasicTypeSingle(Resource, ABC):
     """Method to handle post requests for table when specific type requested."""
     model_class = None
 
     @auth.login_required
-    def get(self, param):
+    def get(self, id):
         row = app.session.query(self.model_class)\
-            .where(self.model_class.id == param)
+            .where(self.model_class.id == id).first()
 
-        if row.count() == 0:
+        if not row:
             error_message = f'No information found on {self.__class__.__name__} found'
             app.logger.debug(f'[{request.authorization.username}] {error_message}')
 
-            return jsonify(error=error_message), 401
+            return dict(error=error_message), 401
 
         app.logger.debug(
             f'[{request.authorization.username}] '
-            f'Returning {self.__class__.__name__} id {param}')
-        return row[0].to_json()
+            f'Returning {self.__class__.__name__} id {id}')
+        return row.to_json()
 
     @auth.login_required
-    def post(self, param):
+    def put(self, id):
         """Method to handle insert of new type.
 
         :param param: type description
         :return: type id that was inserted
         """
         row = app.session.query(self.model_class)\
-            .where(self.model_class.description == param)
+            .where(self.model_class.id == id).first()
 
-        if row.count() > 0:
-            return jsonify(
-                error=f'[{request.authorization.username}] '
-                      f'{self.__class__.__name__} id already exists',
-                id=row[0].id), 401
+        if not row:
+            return dict(
+                error=f'{self.__class__.__name__} id not exists',
+                id=id), 401
 
-        max_id = app.session.query(func.max(self.model_class.id))[0]
-        next_id = int(max_id[0]) + 1
-        app.session.bulk_save_objects([self.model_class(next_id, param)])
+        data = json.loads(request.get_data().decode('utf-8'))
+
+        if 'description' not in data:
+            return {'error': 'Invalid description provided'}, 401
+
+        row.description = data['description']
+
+        app.session.bulk_save_objects([row])
         app.session.commit()
 
         message = f"{self.__class__.__name__}:" + \
-                  f" {next_id} created successfully"
+                  f" {row.id} saved successfully"
         app.logger.debug(f"[{request.authorization.username}] " + message)
 
-        return jsonify(message=message, database_id=next_id)
+        return dict(message=message, register=row.to_json())
+
+    @auth.login_required
+    def delete(self, id):
+        """Method to handle deleted requests for type tables."""
+        row = app.session.query(self.model_class)\
+            .where(self.model_class.id == id).first()
+
+        if not row:
+            return dict(
+                error=f'{self.__class__.__name__} id not exists',
+                id=id), 401
+
+        app.session.delete(row)
+        app.session.commit()
+
+        return dict(success=f'{id} deleted')
+
+    @auth.login_required
+    def options(self, id):
+        """Method to handle options requests for type tables."""
+        return dict(Allow=['DELETE', 'PUT', 'GET'])
 
 
 class DatabaseTypes(BasicTypes):
@@ -146,15 +196,8 @@ class Users(BasicTypes):
     """Class to handle user requests"""
     model_class = models.UserModel
 
-
-class User(BasicTypeSingle):
-    """Class to handle user single request"""
-    model_class = models.UserModel
-
-    @app.route('/user', methods=['POST'])
     @auth.login_required
-    @staticmethod
-    def post():
+    def post(self):
         """Method to handle the user insert"""
         name = request.headers.get('name', None)
         password = request.headers.get('password', None)
@@ -190,6 +233,10 @@ class User(BasicTypeSingle):
 
         return {'success': f'User {name} created.', 'id': row.id}
 
+
+class User(BasicTypeSingle):
+    """Class to handle user single request"""
+    model_class = models.UserModel
 
 
 class UserGroups(BasicTypes):
