166f111589ae45c1c68037e851120e8deee24d9b
marinellirubens <marinelli.rubens@gmail.com>
2022-08-25 05:53:58 -0300

    Changed the usage of methods

--- server/utils.py
+++ server/utils.py
@@ -7,7 +7,7 @@ from flask_restful import Api
 
 from server import resources
 from server.app import App
-    
+
 
 def create_app(app_name: str, database_directory: str = 'sqlite',
                log_dir: str = './logs', directory_files: str = './files') -> Flask:
@@ -22,26 +22,27 @@ def create_app(app_name: str, database_directory: str = 'sqlite',
     app.engine = engine
     app.session = session
 
-    api.add_resource(resources.DatabaseType,
-                     '/database_type/<param>',
-                     methods=['GET', 'POST', 'DELETE'])
-    api.add_resource(resources.DatabaseTypes, '/database_types/', methods=['GET'])
+    basic_methods = ['GET', 'POST', 'OPTIONS']
+    individual_methods = ['GET', 'PUT', 'OPTIONS', 'DELETE']
 
-    api.add_resource(resources.ConnectionTypes, '/connection_types/', methods=['GET'])
-    api.add_resource(resources.ConnectionType, '/connection_type/<param>', methods=['GET', 'POST'])
+    api.add_resource(resources.DatabaseTypes, '/database_types/', methods=basic_methods)
+    api.add_resource(resources.DatabaseType, '/database_types/<id>', methods=individual_methods)
 
-    api.add_resource(resources.ServerTypes, '/server_types/', methods=['GET'])
-    api.add_resource(resources.ServerType, '/server_type/<param>', methods=['GET', 'POST'])
+    api.add_resource(resources.ConnectionTypes, '/connection_types/', methods=basic_methods)
+    api.add_resource(resources.ConnectionType, '/connection_types/<id>', methods=individual_methods)
 
-    api.add_resource(resources.FunctionTypes, '/function_types/', methods=['GET'])
-    api.add_resource(resources.FunctionType, '/function_type/<param>', methods=['GET', 'POST'])
+    api.add_resource(resources.ServerTypes, '/server_types/', methods=basic_methods)
+    api.add_resource(resources.ServerType, '/server_types/<id>', methods=individual_methods)
 
-    api.add_resource(resources.Groups, '/groups/', methods=['GET'])
-    api.add_resource(resources.Group, '/group/<param>', methods=['GET', 'POST'])
+    api.add_resource(resources.FunctionTypes, '/function_types/', methods=basic_methods)
+    api.add_resource(resources.FunctionType, '/function_types/<id>', methods=individual_methods)
 
-    api.add_resource(resources.Users, '/users/', methods=['GET'])
-    api.add_resource(resources.User, '/user/<param>', methods=['GET', 'OPTIONS'])
-    
-    api.add_resource(resources.UserGroups, '/user_groups/', methods=['GET'])
+    api.add_resource(resources.Groups, '/groups/', methods=basic_methods)
+    api.add_resource(resources.Group, '/groups/<id>', methods=individual_methods)
+
+    api.add_resource(resources.Users, '/users/', methods=basic_methods)
+    api.add_resource(resources.User, '/users/<id>', methods=individual_methods)
+
+    api.add_resource(resources.UserGroups, '/user_groups/', methods=basic_methods)
 
     return app
